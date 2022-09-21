1b93c08502ee2aa7ccfc16b6a651f52d8a7c88fa
Ragnar <ragnar.naruski@voco.ee>
2022-09-21 11:25:43 +0300

    kuni 1.5

---
+++ variables/app.js
@@ -0,0 +1,34 @@
+//1.1
+console.log("tere maailm")
+// console.log(season+" "+numTwo)
+
+//president 1.2
+let aasta=2016
+let president='Kersti Kaljulaid'
+let lauseKeskosa='. aastal valiti Eesti presidendiks '
+let lause=aasta+lauseKeskosa+president
+console.log(lause)
+
+//astendamine 1.3
+// let astmeAlus=prompt('sisesta astme alus')
+// let astendaja=prompt('sisesta astendaja')
+// let tulemus= Math.pow(astmeAlus, astendaja)
+//
+// console.log(tulemus)
+
+//1.4
+// let nimi=prompt('nimi')
+// let lubatubKiirus=Number(prompt('lubatud kiirus'))
+// let tegelikKiirus=Number(prompt('tegelik kiirus'))
+// let trahv=(tegelikKiirus-lubatubKiirus)*3
+//
+// console.log(nimi+", kiiruse yletamise eest on teie trahv "+trahv+" eurot.")
+
+//1.5
+let ainepunktid=3
+nadalad=10
+tunnid=ainepunktid*26
+console.log(tunnid)
+toimetan=tunnid/nadalad
+console.log(Math.ceil(toimetan))
+
