60e31e243db6c20a0ce0a033abecd175abca0553
Ragnar <ragnar.naruski@voco.ee>
2022-09-22 14:38:28 +0300

    1.6

--- variables/app.js
+++ variables/app.js
@@ -1,34 +1,13 @@
-//1.1
-console.log("tere maailm")
-// console.log(season+" "+numTwo)
+//1.6
+let inimesteArv=prompt("inimeste arv")
+let bussiKohad=prompt("kohtade arv yhes bussis")
 
-//president 1.2
-let aasta=2016
-let president='Kersti Kaljulaid'
-let lauseKeskosa='. aastal valiti Eesti presidendiks '
-let lause=aasta+lauseKeskosa+president
-console.log(lause)
+let bussidTaidetud=Math.floor(inimesteArv/bussiKohad)
+let mahaJaanud= inimesteArv%bussiKohad
+let bussiPealeJ6udsid=Math.floor(inimesteArv/bussiKohad)*bussiKohad
 
-//astendamine 1.3
-// let astmeAlus=prompt('sisesta astme alus')
-// let astendaja=prompt('sisesta astendaja')
-// let tulemus= Math.pow(astmeAlus, astendaja)
-//
-// console.log(tulemus)
-
-//1.4
-// let nimi=prompt('nimi')
-// let lubatubKiirus=Number(prompt('lubatud kiirus'))
-// let tegelikKiirus=Number(prompt('tegelik kiirus'))
-// let trahv=(tegelikKiirus-lubatubKiirus)*3
-//
-// console.log(nimi+", kiiruse yletamise eest on teie trahv "+trahv+" eurot.")
-
-//1.5
-let ainepunktid=3
-nadalad=10
-tunnid=ainepunktid*26
-console.log(tunnid)
-toimetan=tunnid/nadalad
-console.log(Math.ceil(toimetan))
-
+console.log("Inimeste arv on "+inimesteArv+" bussis on "+bussiKohad+" kohta. Busse on "+bussidTaidetud+", bussidesse mahtus "+bussiPealeJ6udsid+".")
+if (mahaJaanud !=0){
+    console.log("Maha j2i "+mahaJaanud+".")
+}
+// == != < > <= >= tru/fal
